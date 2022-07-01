f159dde99ee5228e5d5eff8beb0dab573e21379f
Adedoyin <adebajoa15@gmail.com>
2022-07-01 10:35:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="header-class">
+     <hi>Basic react application</hi>
+    </div>
+  );
+}
+
+export default App;
